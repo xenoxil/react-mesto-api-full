2515fce7fff68e2ff4da902270e33fc46141f89d
Xenoxil <xilus1127@gmail.com>
2021-05-23 19:58:44 +0300

    очередные правки cors3

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ const limiter = rateLimit({
 });
 
 app.use(cors({
-  origin: true,
+  origin: ['https://xenoxil.mesto.nomoredomains.icu', 'https://xenoxil.mesto.nomoredomains.icu/sign-in', 'https://xenoxil.mesto.nomoredomains.icu/sign-up'],
   credentials: true,
 }));
 
