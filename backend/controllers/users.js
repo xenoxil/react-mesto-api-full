6f487be7011c1fd680321dc850e19c6c9df40139
Xenoxil <xilus1127@gmail.com>
2021-05-23 15:47:40 +0300

    настройка куки

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -90,7 +90,7 @@ module.exports.login = (req, res, next) => {
               },
             );
             res
-              .cookie('mestoToken', token, { maxAge: 604800000, httpOnly: true, sameSite: 'none' })
+              .cookie('mestoToken', token, { maxAge: 604800000, httpOnly: true, sameSite: 'none', secure: true })
               .send({ _id: req.body._id });
           }
         });
